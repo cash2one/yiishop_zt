98663902a3a32a2f43a8efc0830ed4fa9d8c72f4
kobe-123 <1939279208@qq.com>
2018-01-11 14:28:30 +0800

    修改5

---
+++ common/config/main-local.php
@@ -0,0 +1,28 @@
+<?php
+return [
+    'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=127.0.0.1;dbname=project',
+            'username' => 'root',
+            'password' => 'root',
+            'charset' => 'utf8',
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'transport' => [
+              'class' => 'Swift_SmtpTransport',
+              'host' => 'smtp.qq.com',
+              'username' => '1939279208@qq.com',
+              'password' => 'lejsndxfqytlbidf',
+              'port' => '465',
+              'encryption' => 'ssl',
+          ],
+            'viewPath' => '@common/mail',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => false,
+        ],
+    ],
+];
